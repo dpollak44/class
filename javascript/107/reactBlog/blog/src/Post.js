3a71c1e4955655ad13933a6c876c12e716acc53c
slubowsky <270589+slubowsky@users.noreply.github.com>
2020-02-26 22:24:19 -0500

    class 107

---
+++ javascript/107/reactBlog/blog/src/Post.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './Post.css';
+import AddComment from './AddComment';
+
+export default class Post extends Component {
+  state = {};
+
+  render() {
+    const { post } = this.props;
+
+    const addCommentButton = this.state.addingComment ?
+      null :
+      <button className="addComment" onClick={this.addComment}>add comment</button>;
+
+    const addComment = this.state.addingComment ?
+      <AddComment onComplete={this.commentComplete} id={post._id} /> :
+      null;
+
+    return (
+      <div className="post" id={post._id}>
+        <h2>{post.title}</h2>
+        <h3>by {post.author} on {new Date(post.date).toLocaleString()}</h3>
+        <div>{post.content}</div>
+
+        <div className="comments">
+          {addCommentButton}
+          {addComment}
+        </div>
+      </div>
+    );
+  }
+
+  addComment = () => {
+    this.setState({
+      addingComment: !this.state.addingComment
+    });
+  }
+
+  commentComplete = () => {
+    this.setState({
+      addingComment: false
+    });
+  }
+}
